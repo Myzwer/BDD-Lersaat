824426245e77be3d5f2af0ed6ed3d835a2cb932d
Myzwer <josh@onefortyfivedesign.com>
2023-03-21 01:15:55 -0400

    :art: Updated the bride page to look like the mockup

--- pages/bride.vue
+++ pages/bride.vue
@@ -1,83 +1,97 @@
 <template>
     <div>
-        <div class="relative">
-            <div class="bride-h1-background">
-                <div class="content-middle text-white text-center">
-                    <h1 class="text-7xl mb-2 px-2">Meet The Bride</h1>
-                    <h2 class="text-xl font-bold uppercase body-font">
-                        {{ hashtag }}
-                    </h2>
-                </div>
-            </div>
-        </div>
+		<!-- START HEADER -->
+		<div class="bg-primary bg-gradient-to-b from-primary to-primary-gradient text-white">
+			<div class="grid grid-cols-12">
+				<div class="col-span-12 md:col-span-6 md:order-2">
+					<div class="relative">
+						<div class="title-content-text">
+							<div class="content-middle text-center">
+								<h1 class="text-7xl mb-2 px-2">Meet The Bride</h1>
+								<h2 class="text-xl font-bold uppercase body-font">
+									{{ hashtag }}
+								</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-span-12 md:col-span-6">
+					<div class="relative">
+						<div class="half-content-bride-header">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- END HEADER -->
 
-        <div class="bg-secondary">
-            <div class="text-center mx-auto max-w-2xl md:max-w-5xl">
-                <div class="grid grid-cols-12 gap-4 pt-10 pb-12">
-                    <div class="col-span-12 md:col-span-5 px-5">
-                        <div class="mt-10 px-5">
-                            <img
-                                class="rounded-xl shadow-lg"
-                                :src="headshot"
-                                alt="Bride Headshot"
-                            />
-                        </div>
-                    </div>
+		<div class="bg-secondary bg-gradient-to-b from-secondary to-secondary-gradient">
+			<div class="text-center mx-auto max-w-2xl md:max-w-3xl">
+				<div class="grid grid-cols-12 gap-4 pt-10 pb-12">
+					<div class="col-span-12 px-5">
+						<h2 class="pt-7 text-5xl text-center pb-5">
+							Hey! I'm {{ firstName }}.
+						</h2>
+						<p class="text-left pb-5">
+							{{ bio1 }}
+						</p>
 
-                    <div class="col-span-12 md:col-span-7 px-5">
-                        <h2 class="pt-7 text-5xl text-left">
-                            Hey! I'm {{ firstName }}.
-                        </h2>
-                        <p class="text-left pb-5">
-                            {{ bio1 }}
-                        </p>
+						<p class="text-left pb-5">
+							{{ bio2 }}
+						</p>
 
-                        <p class="text-left pb-5">
-                            {{ bio2 }}
-                        </p>
-                        <div v-if="facebook != null" class="pb-5">
-                            <a :href="facebook" target="_blank">
-                                <div
-                                    class="text-black border-2 border-black uppercase rounded-md font-bold shadow-lg px-8 py-3 ease-in-out duration-300 hover:bg-accent hover:text-white hover:border-white"
-                                >
-                                    <font-awesome-icon
-                                        icon="fa-brands fa-facebook"
-                                    />
-                                    Facebook
-                                </div>
-                            </a>
-                        </div>
-                        <div v-if="instagram != null" class="pb-5">
-                            <a :href="instagram" target="_blank">
-                                <div
-                                    class="text-black border-2 border-black uppercase rounded-md font-bold shadow-lg px-8 py-3 ease-in-out duration-300 hover:bg-accent hover:text-white hover:border-white"
-                                >
-                                    <font-awesome-icon
-                                        icon="fa-brands fa-instagram"
-                                    />
-                                    Instagram
-                                </div>
-                            </a>
-                        </div>
-                        <div v-if="twitter != null" class="pb-5">
-                            <a :href="twitter" target="_blank">
-                                <div
-                                    class="text-black border-2 border-black uppercase rounded-md font-bold shadow-lg px-8 py-3 ease-in-out duration-300 hover:bg-accent hover:text-white hover:border-white"
-                                >
-                                    <font-awesome-icon
-                                        icon="fa-brands fa-twitter"
-                                    />
-                                    Twitter
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+						<div class="grid grid-cols-12 gap-4">
+							<div class="col-span-12 md:col-span-4">
+								<div v-if="facebook != null" class="pb-5">
+									<a :href="facebook" target="_blank">
+										<div
+											class="text-black border-2 border-black uppercase rounded-md font-bold shadow-lg px-8 py-3 ease-in-out duration-300 hover:bg-accent"
+										>
+											<font-awesome-icon
+												icon="fa-brands fa-facebook"
+											/>
+											Facebook
+										</div>
+									</a>
+								</div>
+							</div>
+							<div class="col-span-12 md:col-span-4">
+								<div v-if="instagram != null" class="pb-5">
+									<a :href="instagram" target="_blank">
+										<div
+											class="text-black border-2 border-black uppercase rounded-md font-bold shadow-lg px-8 py-3 ease-in-out duration-300 hover:bg-accent"
+										>
+											<font-awesome-icon
+												icon="fa-brands fa-instagram"
+											/>
+											Instagram
+										</div>
+									</a>
+								</div>
+							</div>
 
-        <button-footer />
-    </div>
+							<div class="col-span-12 md:col-span-4">
+								<div v-if="twitter != null" class="pb-5">
+									<a :href="twitter" target="_blank">
+										<div
+											class="text-black border-2 border-black uppercase rounded-md font-bold shadow-lg px-8 py-3 ease-in-out duration-300 hover:bg-accent"
+										>
+											<font-awesome-icon
+												icon="fa-brands fa-twitter"
+											/>
+											Twitter
+										</div>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<button-footer />
+	</div>
 </template>
 
 <script>
@@ -104,13 +118,19 @@ export default {
 
 <style>
 /* stylelint-disable color-function-notation */
-.bride-h1-background {
-    background: linear-gradient(rgba(0, 0, 0, 60%), rgba(0, 0, 0, 60%)),
-        url('~static/images/bride/bride-head.jpg');
-    height: 60vh;
-    background-position: center center;
-    background-repeat: no-repeat;
-    background-size: cover;
+.half-content-bride-header {
+	background: linear-gradient(rgba(0, 0, 0, 0%), rgba(0, 0, 0, 0%)),
+	url('~static/images/bride/bride-head.jpg');
+	height: 25vh;
+	background-position: center center;
+	background-repeat: no-repeat;
+	background-size: cover;
+}
+
+@media screen and (min-width: 40em) {
+	.half-content-bride-header {
+		height: 75vh;
+	}
 }
 
 /* stylelint-enable color-function-notation */
