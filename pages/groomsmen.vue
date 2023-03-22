08cb9e06cb90d9163bd36e493e2b10cf2c023945
Myzwer <josh@onefortyfivedesign.com>
2023-03-22 01:55:55 -0400

    :art: Knocked out the groomsmen page. it looks like the mockup.

--- pages/groomsmen.vue
+++ pages/groomsmen.vue
@@ -1,15 +1,29 @@
 <template>
     <div>
-        <div class="relative">
-            <div class="groomsmen-h1-background">
-                <div class="content-middle text-white text-center">
-                    <h1 class="text-7xl mb-2 px-2">Meet The Guys</h1>
-                    <h2 class="text-xl font-bold uppercase body-font">
-                        {{ hashtag }}
-                    </h2>
-                </div>
-            </div>
-        </div>
+		<!-- START HEADER -->
+		<div class="bg-primary bg-gradient-to-b from-primary to-primary-gradient text-white">
+			<div class="grid grid-cols-12">
+				<div class="col-span-12 md:col-span-6 md:order-2">
+					<div class="relative">
+						<div class="title-content-text">
+							<div class="content-middle text-center">
+								<h1 class="text-7xl mb-2 px-2">Meet The Guys</h1>
+								<h2 class="text-xl font-bold uppercase body-font">
+									{{ hashtag }}
+								</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-span-12 md:col-span-6">
+					<div class="relative">
+						<div class="half-content-groomsmen-header">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- END HEADER -->
 
         <SingleBio
             v-for="(bio, index) in groomsmen"
@@ -24,13 +38,30 @@
             :description="bio.description"
         />
 
+		<div class="bg-primary text-white">
+			<div class="grid grid-cols-12">
+				<div class="col-span-12 text-center py-10">
+					<h1 class="body-font text-3xl uppercase pb-10">See You There?</h1>
+					<div class="text-center mb-5">
+						<a
+							:href="rsvpLink"
+							target="_blank"
+							class="bg-accent text-black uppercase rounded-md font-bold shadow-lg px-20 py-3 transition duration-300 ease-in-out hover:bg-accent-alt mt-10"
+						>
+							RSVP!
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
         <button-footer />
     </div>
 </template>
 
 <script>
 import SingleBio from '~/components/SingleBio'
-import { groomsmen, tagline } from '~/data/data.json'
+import { groomsmen, tagline, RSVP } from '~/data/data.json'
 export default {
     name: 'MeetTheGroomsmen',
     components: { SingleBio },
@@ -38,21 +69,20 @@ export default {
         return {
             groomsmen,
             hashtag: tagline.hashtag,
+			rsvpLink: RSVP.link,
         }
     },
 }
 </script>
 
 <style>
-/* stylelint-disable color-function-notation */
-.groomsmen-h1-background {
-    background: linear-gradient(rgba(0, 0, 0, 60%), rgba(0, 0, 0, 60%)),
-        url('~static/images/groomsmen/groomsmen-head.jpg');
-    height: 60vh;
-    background-position: top center;
-    background-repeat: no-repeat;
-    background-size: cover;
+@media screen and (min-width: 40em) {
+	.half-content-groomsmen-header {
+		background: url('~static/images/groomsmen/groomsmen-head.jpg');
+		height: 75vh;
+		background-position: center center;
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
 }
-
-/* stylelint-enable color-function-notation */
 </style>
